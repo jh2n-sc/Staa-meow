9acb888f9bda64c386bfcfb17bfe3ea0cbbde55e
jh2n-sc <djcat211@gmail.com>
2024-12-20 04:01:58 +0800

    Fixed LogInController

---
+++ src/com/sta/SignUpSuccessController.java
@@ -0,0 +1,15 @@
+package com.sta;
+
+import com.sta.utility.Utility;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SignUpSuccessController {
+
+    @FXML
+    public void onOKClicked(ActionEvent actionEvent) {
+        Utility.switchScene("/fxml/main.fxml", "Sta Meow Login", actionEvent);
+
+    }
+}
